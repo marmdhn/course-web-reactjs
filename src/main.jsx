51c91b11fb054c6215cf5cc9abdd9225cef38890
Ramadhan <makbarramadhan62@gmail.com>
2024-07-18 11:20:23 +0700

    refactor: adjust import url

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "/dist/css/main.css";
+import "../dist/css/main.css";
 import "animate.css";
 
 import AOS from "aos";
